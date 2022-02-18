2431bb8ed33f9bb1005aae48ad3f10ea81099a24
Luke Hayes <lukepurplehayes@gmail.com>
2022-02-18 19:01:06 +0000

    Added GetStoryChoices() function to project.

--- include/io.h
+++ include/io.h
@@ -4,6 +4,10 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+#include <regex>
+
+using VecStr = std::vector<std::string>;
 
 std::string ReadFile(const std::string& filename)
 {
@@ -20,5 +24,28 @@ std::string ReadFile(const std::string& filename)
     stream.close();
     return fileContents;
 }
+
+/**
+ * Get all of the possible choices for the current story page
+ */
+VecStr GetStoryChoices()
+{
+    VecStr choices;
+
+    std::string str = ReadFile("pages/1.txt");
+    std::cout << "Contents: " << str << std::endl;
+    std::smatch match;
+    std::regex r("(\\[[A-Z]\\])"); // Grab the capital letter in "[W]alk"
+
+    std::regex_search(str, match, r);
+    choices.push_back(match.str());
+
+    //std::cout << match.suffix().str() << std::endl;
+    //std::cout << match.str() << std::endl;
+
+
+    return choices;
+}
+
 
 #endif
