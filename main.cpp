f7806d0007045ef3599ce1dc50588899226a1f28
Luke Hayes <lukepurplehayes@gmail.com>
2022-02-18 19:02:28 +0000

    Updated main to latest version.

--- main.cpp
+++ main.cpp
@@ -9,8 +9,16 @@ int main()
     std::cout << "Welcome." << std::endl;
     std::string input;
 
-    std::string file = ReadFile("scripts/1.txt");
+    std::string file = ReadFile("pages/1.txt");
     std::cout << file << std::endl;
+    
+    for(auto choice : GetStoryChoices())
+    {
+        static int i = 1;
+        std::cout << i << ": " << choice  << std::endl;
+        i++;
+    }
+
 
     while(input != "q")
     {
